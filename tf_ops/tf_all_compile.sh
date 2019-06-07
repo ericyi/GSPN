6d59311d952f36cfd89e0ebe72caf6bb20053017
Eric Yi <ericyi@oriong4.stanford.edu>
2019-06-07 14:53:43 -0700

    first commit

---
+++ tf_ops/tf_all_compile.sh
@@ -0,0 +1,9 @@
+cd ./grouping
+sh ./tf_grouping_compile.sh
+cd ../3d_interpolation
+sh ./tf_interpolate_compile.sh
+cd ../nn_distance
+sh ./tf_nndistance_compile.sh
+cd ../sampling
+sh ./tf_sampling_compile.sh
+cd ..
